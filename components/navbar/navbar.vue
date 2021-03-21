8625d50bded3c174e2a3d8bbbd996b5febdf60ec
sdpsdpsdp <3144518586@qq.com>
2021-03-21 22:55:37 +0800

    init

---
+++ components/navbar/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		导航栏
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"navbar",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/tabbar/follow/follow.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		Follow
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
