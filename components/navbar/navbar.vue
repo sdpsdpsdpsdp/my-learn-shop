1b6bce874a0d1230bd011b3850c15eab37a9bd65
Panda <809263087@qq.com>
2021-03-22 20:09:35 +0800

    完成测试

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,61 +1,86 @@
 <template>
 	<view class="navbar">
 		<view class="navbar-fixed">
-			<view class="navbar-search">
-				<view class="navbar-search_icon"></view>
-				<view class="navbar-search_text">搜索消息</view>
+			<view :style="{height: statusHeight+'px'}"></view>
+			<view class="navabar-content" :style="{width:windowWidth+'px', height: navbarHeight+'px'}">
+				<view class="navbar-search">
+					<view class="navbar-search_icon">
+						<text class="iconfont icon-search"></text>
+					</view>
+					<view class="navbar-search_text">搜索消息</view>
+				</view>
 			</view>
 		</view>
-		<view style="height: 45px;"></view>
+		<view :style="{height: (navbarHeight+statusHeight)+'px'}"></view>
 	</view>
 </template>
 
 <script>
 	export default {
-		name:"navbar",
+		name: "navbar",
 		data() {
 			return {
-				
+				statusHeight: 20,
+				navbarHeight:45,
+				windowWidth:375
 			};
+		},
+		created() {
+			//获取手机系统信息
+			const systemInfo = uni.getSystemInfoSync()
+			this.statusHeight = systemInfo.statusBarHeight
+			this.windowWidth = systemInfo.windowWidth
+			//#ifndef H5 || APP-PLUS ||MP-ALIPAY
+			//获取胶囊的位置信息
+			const menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+			//胶囊底部高度-状态栏的高度 +（胶囊顶部的高度-状态栏内的高度）
+			this.navbarHeight = (menuButtonInfo.bottom-this.statusHeight)+(menuButtonInfo.top-this.statusHeight)
+			this.windowWidth = menuButtonInfo.left
+			//#endif
 		}
 	}
 </script>
 
 <style lang="scss">
-	.navbar{
-		.navbar-fixed{
+	@import "../../common/css/icons.css";
+	.navbar {
+		display: flex;
+		flex-direction: column;
+		.navbar-fixed {
 			position: fixed;
 			top: 0;
 			left: 0;
 			z-index: 99;
-			display: flex;
 			width: 100%;
-			height: 45px;
-			justify-content: center;
-			align-items: center;
-			padding: auto 15px;
-			background-color:$mk-base-color;
-			.navbar-search{
-				height: 30px;
-				width: 100%;
-				margin: auto 10px;
-				background-color: #FFFFFF;
+			background-color: $mk-base-color;
+
+			.navabar-content {
 				display: flex;
+				justify-content: center;
 				align-items: center;
-				border-radius: 15px;
-				.navbar-search_icon{
-					margin-left: 15px;
-					margin-right: 10px;
-					width: 10px;
-					height: 10px;
-					border: 1px red solid;
-				}
-				.navbar-search_text{
-					font-size: 12px;
-					color: #808080;
+				padding: 0 15px;
+				height: 45px;
+				box-sizing: border-box;
+
+				.navbar-search {
+					display: flex;
+					align-items: center;
+					padding: 0 10px;
+					width: 100%;
+					height: 30px;
+					border-radius: 15px;
+					background-color: #FFFFFF;
+
+					.navbar-search_icon {
+						margin-right: 10px;
+					}
+
+					.navbar-search_text {
+						font-size: 12px;
+						color: #808080;
+					}
 				}
 			}
 		}
-		
 	}
 </style>
